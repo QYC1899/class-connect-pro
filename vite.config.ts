8ca65faf681f03eec6c056515b3a56e44aaa9c93
QYC1899 <quek.yuchen@gmail.com>
2026-07-26 00:39:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    allowedHosts: [
+      "sb-1mlr1awdl7e7.vercel.run"
+    ]
     host: "::",
     port: 8080,
     hmr: {
